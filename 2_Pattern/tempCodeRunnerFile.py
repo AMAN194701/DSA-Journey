cf9d66837f6c77a2ed18faa0f718f7bd83a226bc
AMAN KUSHWAHA <amankushwaha@AMANs-MacBook-Air.local>
2026-03-11 11:33:36 +0530

    Reorganize project structure: move Basic_Program and 2_Pattern into Basics folder

--- 2_Pattern/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-print("To print inverse Alphabet Triangle Pattern ")
-n=int(input("Enter the number of rows : "))
-for row in range(n,0,-1):
-    for col in range(row):
-        print(chr(65+col), end="")
-    print()
